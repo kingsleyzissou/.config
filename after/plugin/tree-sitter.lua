aae5ae2f457cfea665bceb60e362229803efdee8
Gianluca Zuccarelli <gzuccare@redhat.com>
2023-01-13 12:30:36 +0000

    update tree-sitter

--- after/plugin/tree-sitter.lua
+++ after/plugin/tree-sitter.lua
@@ -1,6 +1,6 @@
 local treesitter = require('nvim-treesitter.configs')
 
 treesitter.setup({
-    ensure_installed = { 'http', 'json' },
+    ensure_installed = { 'http', 'json', 'go', 'python', 'yaml' },
     auto_install = true,
 })
