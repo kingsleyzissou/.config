a4048deeca25058e338d43ecb958d22ace985f8a
Gianluca Zuccarelli <gzuccare@redhat.com>
2023-01-13 12:25:34 +0000

    update dashboard

--- after/plugin/alpha.lua
+++ after/plugin/alpha.lua
@@ -7,21 +7,16 @@ local function button(sc, txt, keybind, keybind_opts)
     return b
 end
 
--- TODO: sort out icons
 dashboard.section.buttons.val = {
-    button('f', ' Find file', ':Telescope find_files <CR>'),
-    button('e', ' New file', ':ene <BAR> startinsert <CR>'),
-    button('p', ' Find project', ":lua require('telescope').extensions.projects.projects()<CR>"),
-    button('r', ' Recent files', ':Telescope oldfiles <CR>'),
-    button('t', ' Find text', ':Telescope live_grep <CR>'),
-    -- dashboard.button("s", icons.ui.SignIn .. " Find Session", ":silent Autosession search <CR>"),
-    button('s', ' Find Session', ':SearchSession<CR>'),
-    button('c', ' Config', ':e ~/.config/nvim/init.lua <CR>'),
-    button('u', ' Update', ':PackerSync<CR>'),
-    button('q', ' Quit', ':qa<CR>'),
+    button('f', ' Find file', '<cmd>Telescope find_files<cr>'),
+    button('e', ' New file', '<cmd>ene <BAR> startinsert<cr>'),
+    button('r', ' Recent files', '<cmd>Telescope oldfiles<cr>'),
+    button('t', ' Find text', '<cmd>Telescope live_grep<cr>'),
+    button('c', ' Config', '<cmd>e ~/.config/nvim/init.lua<cr>'),
+    button('u', ' Update', '<cmd>PackerSync<cr>'),
+    button('q', ' Quit', '<cmd>qa<cr>'),
 }
 
--- TODO: sort out footer
 dashboard.section.header.opts.hl = 'Keyword'
 dashboard.section.buttons.opts.hl = 'Macro'
 dashboard.section.footer.opts.hl = 'Type'
