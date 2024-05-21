1cfa80bd9038738852feabff82fd8f8412d875f5
Gianluca Zuccarelli <gzuccare@redhat.com>
2024-05-21 12:42:46 +0100

    nvim: remove copilot

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -1,28 +1,5 @@
 return {
   {
-    -- copilot
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    build = ':Copilot auth',
-    event = 'InsertEnter',
-    opts = {
-      suggestion = {
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = '<tab>',
-          accept_word = false,
-          accept_line = false,
-          next = '<m-]>',
-          prev = '<m-[>',
-          dismiss = '<c-c>',
-        },
-        enabled = true,
-      },
-    },
-  },
-
-  {
     -- autocomplete
     'hrsh7th/nvim-cmp',
     dependencies = {
