8256fb1bb00bcbdae3ff69f6fc5d6fc6182f983e
Gianluca Zuccarelli <gzuccare@redhat.com>
2025-01-08 14:08:37 +0000

    nvim: keymaps update

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,15 +1,15 @@
 -- save/source/quit
-vim.keymap.set('n', '<c-s>', '<cmd>w!<cr>', { desc = 'Save' })
+vim.keymap.set('n', '<c-c>', '<cmd>wq!<cr>', { desc = 'Save' })
 vim.keymap.set('n', '<c-q>', '<cmd>Bdelete<cr>', { desc = 'Quit' })
 vim.keymap.set('n', '<leader>qq', '<cmd>q!<cr>', { desc = 'Quit' })
 vim.keymap.set('n', '<leader>ww', '<cmd>w!<cr>', { desc = 'Save' })
 vim.keymap.set('n', '<leader>wq', '<cmd>wq!<cr>', { desc = 'Save & quit' })
 
 -- lazy
-vim.keymap.set('n', '<leader>i', '<cmd>Lazy<cr>', { desc = 'Lazy' })
+vim.keymap.set('n', '<leader>pi', '<cmd>Lazy<cr>', { desc = 'Lazy' })
 
 -- shift-k // split line at cursor
-vim.keymap.set('n', '<s-k>', 'i<cr><esc>', { desc = 'Split line at cursor' })
+vim.keymap.set({ 'n', 'v' }, '<s-k>', 'i<cr><esc>', { desc = 'Split line' })
 
 -- search for word under cursor
 vim.keymap.set({ 'n', 'x' }, 'gw', '*N', { desc = 'Search word under cursor' })
@@ -31,19 +31,16 @@ vim.keymap.set('v', '<', '<gv', { desc = 'Indent left' })
 vim.keymap.set('v', '>', '>gv', { desc = 'Indent right' })
 
 -- copy/paste stuff
-vim.keymap.set('x', '<a-p>', '"_dP') -- delete current word into the void
-vim.keymap.set('n', '<a-y>', '"+y', { desc = 'Copy to system clipboard' })
-vim.keymap.set('n', '<a-Y>', '"+Y', { desc = 'Copy to system clipboard' })
-vim.keymap.set('v', '<a-y>', '"+y', { desc = 'Copy to system clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<a-y>', '"+y', { desc = 'Copy to system clipboard' })
 vim.keymap.set('n', '<a-v>', '"+p"+Y', { desc = 'Paste from system clipboard' })
 vim.keymap.set('v', '<a-v>', 'c<ESC>"+p', { desc = 'Paste from system clipboard' })
 
 -- send deletes to the ether
-vim.keymap.set('n', 'x', '"_x') -- delete into the void
-vim.keymap.set('v', 'x', '"_x') -- delete into the void
+vim.keymap.set('x', '<a-p>', '"_dP') -- delete current word into the void
+vim.keymap.set({ 'n', 'v' }, 'x', '"_x') -- delete char into the void
 
 -- exit terminal
-vim.keymap.set('t', '<esc>', '<c-\\><c-n>', { desc = 'Esc terminal' })
+vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>', { desc = 'Esc terminal' })
 vim.keymap.set('t', '<c-q>', '<c-\\><c-n><cmd>q!<cr>', { desc = 'Close terminal' }) -- ctrl-q to exit terminal
 
 -- move lines
@@ -61,11 +58,7 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Jump half page up' })
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
--- rest nvim
-vim.keymap.set('n', '<leader>rr', '<Plug>RestNvim', { desc = 'RestNvim' })
-
 -- https://github.com/ThePrimeagen/init.lua/blob/master/lua/theprimeagen/remap.lua
 -- stylua: ignore
 local replace = [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<left><left><left>]]
-vim.keymap.set('n', '<leader>s', replace, { desc = 'Substitute word under cursor' })
-vim.keymap.set('n', '<a-x>', '<cmd>!chmod +x %<cr>', { silent = true, desc = 'Make executable' })
+vim.keymap.set('n', '<leader>r', replace, { desc = 'Replace word' })
