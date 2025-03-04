691d9a2eebc8b8bb375b25f329f0e2c9bde00329
Gianluca Zuccarelli <gzuccare@redhat.com>
2025-03-04 19:20:00 +0000

    nvim: minimal language servers

--- .config/nvim/lua/plugins/lsp/servers/init.lua
+++ .config/nvim/lua/plugins/lsp/servers/init.lua
@@ -1,23 +1,12 @@
 return {
-  -- go
-  golangci_lint_ls = {},
-  gopls = require('plugins.lsp.servers.gopls'),
-
-  -- python
-  jedi_language_server = {},
-
   -- lua
   lua_ls = require('plugins.lsp.servers.lua_ls'),
 
   -- typescript/javascript
   ts_ls = {},
-  -- eslint = {},
-
-  -- rust
-  rust_analyzer = {},
 
   -- other
   cssls = {},
   jsonls = {},
-  -- yamlls = {},
+  yamlls = {},
 }
