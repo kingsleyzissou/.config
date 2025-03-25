5d3e75ec65a3ff87e34297f4b7fd3c7b1a6a7f2a
Gianluca Zuccarelli <gzuccare@redhat.com>
2025-03-25 17:37:02 +0000

    ags: handle display changes

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -1,14 +1,32 @@
-import { App } from 'astal/gtk3';
+import { App, Gdk } from 'astal/gtk3';
 
 import styles from './styles/style.scss';
 import { Widgets } from './src/widgets';
 import { requestHandler } from './src/requests/handler';
+
+const onDisplayChange = () => {
+  const display = Gdk.Display.get_default();
+  display?.connect('monitor-added', (_, monitor) => {
+    Widgets.map((window) => App.add_window(window(monitor)));
+  });
+
+  display?.connect('monitor-removed', (_, monitor) => {
+    App.get_windows().forEach((window) => {
+      if (window.gdkmonitor === monitor) {
+        App.remove_window(window);
+      }
+    });
+  });
+};
 
 App.start({
   css: styles,
   icons: '~/.local/share/icons',
   requestHandler,
   main() {
-    Widgets.map((window) => App.get_monitors().map(window));
+    Widgets.map((window) => {
+      App.get_monitors().map(window);
+    });
+    onDisplayChange();
   },
 });
