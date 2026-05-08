fae5d5733b645581d01758fcb2b4aca348839b3b
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2026-05-08 16:08:01 +0100

    nvim: install hardtimes
    This is a temporary one, but it should help improve with vim motions and
    break some bad habits like spamming the direction keys.

--- base/.config/nvim/lua/plugins/editor/motions.lua
+++ base/.config/nvim/lua/plugins/editor/motions.lua
@@ -9,11 +9,19 @@ return {
   },
 
   {
+    -- the idea here is to use this to punish bad habits and then remove
+    -- should be able to remove precogition after this too
+    'm4xshen/hardtime.nvim',
+    lazy = false,
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {},
+  },
+
+  {
     'folke/flash.nvim',
     event = 'VeryLazy',
     vscode = true,
     opts = {},
-    -- stylua: ignore
     keys = {
       { 's', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash' },
       { 'S', mode = { 'n', 'o', 'x' }, function() require('flash').treesitter() end, desc = 'Flash Treesitter' },
